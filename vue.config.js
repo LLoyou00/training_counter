0eaf6d786d9889b694165cd861d1f6047e0e1276
yanglei <lloyou00@163.com>
2022-06-15 15:02:03 +0800

    chore:修改网页标题

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   // 跳过检查host
   devServer: { disableHostCheck: true },
+  chainWebpack(config) {
+    config.plugin("html").tap((args) => {
+      args[0].title = "小兔子康复训练计时器";
+      return args;
+    });
+  },
 };
